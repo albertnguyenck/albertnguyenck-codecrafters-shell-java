45ab12c159bbe643037ef0a82c0319ffdd2628c9
Albert <albert.nguyen.ck@gmail.com>
2025-12-27 11:07:52 +0800

    add reading command

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-         System.out.print("$ ");
+        System.out.print("$ ");
+        try (Scanner scanner = new Scanner(System.in)) {
+            String input = scanner.nextLine();
+            switch (input) {
+                default: System.out.println(String.format("%s: command not found", input)); break;
+            }
+        }
     }
 }
