5062497abf431e0f34fe16df4ca73a197c8731b2
Albert <albert.nguyen.ck@gmail.com>
2025-12-27 11:41:01 +0800

    refactor

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,40 @@
-import java.util.Scanner;
+private static final Set<String> SHELL_BUILTINS = Set.of("echo", "type", "exit");
 
-public class Main {
-    public static void main(String[] args) throws Exception {
-        try (Scanner scanner = new Scanner(System.in)) {
-            while (true) {
-                System.out.print("$ ");
-                String prompt = scanner.nextLine();
-                String options[] = prompt.split(" ");
-                String command = options[0];
-                switch (command) {
-                    case "echo":
-                        System.out.println(prompt.replace("echo ", ""));
-                        break;
-                    case "exit":
-                        System.exit(0);
-                    default:
-                        System.out.println(String.format("%s: command not found", prompt));
-                        break;
+public static void main(String[] args) throws Exception {
+    try (Scanner scanner = new Scanner(System.in)) {
+        while (true) {
+            System.out.print("$ ");
+            String prompt = scanner.nextLine().trim();
+            String command = prompt.split(" ")[0];
+            switch (command) {
+                case "echo" -> System.out.println(prompt.split(" ", 2)[1]);
+                case "type" -> {
+                    type(prompt);
                 }
+                case "exit" -> System.exit(0);
+                default -> System.out.println(String.format("%s: command not found", prompt));
             }
         }
     }
+}
+
+private static int type(String prompt) {
+    String cmdToCheck = prompt.split(" ", 2)[1];
+    if (SHELL_BUILTINS.contains(cmdToCheck)) {
+        System.out.println(String.format("%s is a shell builtin", cmdToCheck));
+        return 0;
+    } else {
+        String systemPATH = System.getenv("PATH");
+        String[] paths = systemPATH != null ? systemPATH.split(File.pathSeparator) : new String[0];
+        for (String path : paths) {
+            File file = new File(path, cmdToCheck);
+            if (file.exists() && file.canExecute()) {
+                System.out.println(String.format("%s is %s", cmdToCheck, file.getAbsolutePath()));
+                return 0;
+            }
+        }
+        System.out.println(String.format("%s not found", cmdToCheck));
+    }
+
+    return 0;
 }
