69f786d16fcc2dc2d5f1d75b104e9288dc0d208b
Albert <albert.nguyen.ck@gmail.com>
2025-12-27 23:18:07 +0800

    execute external programs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,17 +12,16 @@ public static void main(String[] args) throws Exception {
                     type(prompt);
                 }
                 case "exit" -> System.exit(0);
-                default -> System.out.println(String.format("%s: command not found", prompt));
+                default -> System.out.println(executeExternalCommand(prompt));
             }
         }
     }
 }
 
-private static int type(String prompt) {
+private static void type(String prompt) {
     String cmdToCheck = prompt.split(" ", 2)[1];
     if (SHELL_BUILTINS.contains(cmdToCheck)) {
         System.out.println(String.format("%s is a shell builtin", cmdToCheck));
-        return 0;
     } else {
         String systemPATH = System.getenv("PATH");
         String[] paths = systemPATH != null ? systemPATH.split(File.pathSeparator) : new String[0];
@@ -30,11 +29,41 @@ private static int type(String prompt) {
             File file = new File(path, cmdToCheck);
             if (file.exists() && file.canExecute()) {
                 System.out.println(String.format("%s is %s", cmdToCheck, file.getAbsolutePath()));
-                return 0;
+                return;
             }
         }
         System.out.println(String.format("%s not found", cmdToCheck));
     }
+}
 
-    return 0;
+private static String executeExternalCommand(String prompt) throws IOException, InterruptedException {
+    String[] parts = prompt.split(" ", 2);
+    String cmdToCheck = prompt.split(" ", 2)[0];
+    String commandArgs = parts.length > 1 ? prompt.split(" ", 2)[1] : "";
+    String systemPATH = System.getenv("PATH");
+    String[] paths = systemPATH != null ? systemPATH.split(File.pathSeparator) : new String[0];
+    for (String path : paths) {
+        File file = new File(path, cmdToCheck);
+        if (file.exists() && file.canExecute()) {
+            ProcessBuilder processBuilder = new ProcessBuilder();
+            processBuilder.command(cmdToCheck, commandArgs);
+            Process process = processBuilder.start();
+            StringBuilder output = new StringBuilder();
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    output.append(line).append("\n");
+                }
+            }
+
+            int exitVal = process.waitFor();
+            if (exitVal == 0) {
+                return output.toString();
+            } else {
+                return String.format("Error: Command exited with code %d", exitVal);
+            }
+        }
+    }
+
+    return String.format("%s: command not found", prompt);
 }
