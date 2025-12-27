afb960a27a543c281b02932a39c5d6e221672772
Albert <albert.nguyen.ck@gmail.com>
2025-12-27 11:16:28 +0800

    add loop

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,11 +2,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.print("$ ");
         try (Scanner scanner = new Scanner(System.in)) {
-            String input = scanner.nextLine();
-            switch (input) {
-                default: System.out.println(String.format("%s: command not found", input)); break;
+            while (true) {
+                System.out.print("$ ");
+                String command = scanner.nextLine();
+                switch (command) {
+                    default:
+                        System.out.println(String.format("%s: command not found", command));
+                        break;
+                }
             }
         }
     }
